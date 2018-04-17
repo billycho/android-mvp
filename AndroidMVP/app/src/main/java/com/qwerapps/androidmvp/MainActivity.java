918d2d285b0a011206681b34a3016d053bd7c7e8
billycho <BillyChristi.Hartanto@mitrais.com>
2018-04-17 21:57:55 +0700

    learn butterknife

--- AndroidMVP/app/src/main/java/com/qwerapps/androidmvp/MainActivity.java
+++ AndroidMVP/app/src/main/java/com/qwerapps/androidmvp/MainActivity.java
@@ -2,15 +2,35 @@ package com.qwerapps.androidmvp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text1)
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BattleOfBastards a = new BattleOfBastards();
-        a.startWar();
+//        BattleOfBastards a = new BattleOfBastards();
+//        a.startWar();
+
+        ButterKnife.bind(this);
+        title.setText("Hello from Butterknife");
+
+    }
+
+    @OnClick(R.id.button)
+    public void submit()
+    {
+        Toast.makeText(MainActivity.this, "Hello from Butterknife OnClick annotation", Toast.LENGTH_SHORT).show();
+
     }
 }
